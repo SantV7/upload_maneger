85e35edbb1fbaf15f9bd07e1b1a3cf408b6678d0
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-09-21 04:22:12 -0300

    install: usign cors to connection between frontend and backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
 import { documentRouter } from './app/routes/document.routes.ts';
 import { commentRouter } from './app/routes/comment.routes.ts';
 
 export const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(documentRouter);
 app.use(commentRouter);
